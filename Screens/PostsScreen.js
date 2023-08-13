f4c2ff71f05f20877d8d1626d4cf1263d9ed38b6
RainWell07 <117939087+RainWell07@users.noreply.github.com>
2023-08-13 10:59:36 +0300

    Create PostsScreen.js

---
+++ Screens/PostsScreen.js
@@ -0,0 +1,4 @@
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { MaterialIcons } from '@expo/vector-icons';
